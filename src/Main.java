d60fe6138e342f9a78d0367de41511783ef6bd2c
Nataliia Kryzhanivska <natallikriz@gmail.com>
2023-02-09 11:04:21 +0200

    added main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double latitude = 50.44982912427808;
+        double longitude = 30.523209651269546;
+
+        System.out.println("Latitude of main post office in Kyiv is " + latitude);
+        System.out.println("Longitude of main post office in Kyiv is " + longitude);
+    }
+}
