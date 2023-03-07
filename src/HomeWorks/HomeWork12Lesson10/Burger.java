55f2a5656490dabace86d7acfe9430e07b3e7210
Nataliia Kryzhanivska <natallikriz@gmail.com>
2023-03-07 12:08:43 +0200

    Add HomeWork 12

---
+++ src/HomeWorks/HomeWork12Lesson10/Burger.java
@@ -0,0 +1,43 @@
+package HomeWorks.HomeWork12Lesson10;
+
+public class Burger {
+
+    String  bun;
+    int meat;
+    char cheese;
+    String greens;
+    String mayonnaise;
+    int doubleMeat;
+
+    public Burger(String bun, int meat, char cheese, String greens, String mayonnaise) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
+
+        System.out.println("Звичайний бургер, склад: булка - " + bun + ", грам м'яса - " + meat + ", класс сиру - "
+                + cheese + ", зелень - " + greens + ", " + mayonnaise);
+    }
+
+    public Burger(String bun, int meat, char cheese, String greens) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+
+        System.out.println("Дієтичний бургер, склад: булка - " + bun + ", грам м'яса - " + meat + ", класс сиру - "
+                + cheese + ", зелень - " + greens);
+    }
+
+    public Burger(int doubleMeat, String bun, char cheese, String greens, String mayonnaise) {
+        this.bun = bun;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
+        this.doubleMeat = doubleMeat;
+
+        System.out.println("Бургер з подвійним м'ясом, склад: булка - " + bun + ", грам м'яса - " + doubleMeat +", клас сиру - "
+                + cheese + ", зелень - " + greens + ", " + mayonnaise);
+    }
+}
---
+++ src/HomeWorks/HomeWork12Lesson10/Main.java
@@ -0,0 +1,12 @@
+package HomeWorks.HomeWork12Lesson10;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Burger regularBurger = new Burger("біла", 150, 'А', "салат", "майонез");
+
+        Burger dietBurger = new Burger("чорна", 150, 'А', "салат");
+
+        Burger doubleMeatBurger = new Burger(300, "біла",'А', "салат", "майонез");
+    }
+}
